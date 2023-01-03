28227fb20274054ce72492aa8a9cb46c2fa3d006
StjepanBM1 <stjepan.bilicmatsic546@gmail.com>
2023-01-03 16:03:20 +0100

    bug-fix

--- main.js
+++ main.js
@@ -3,29 +3,25 @@ const readline = require('readline').createInterface({
   output: process.stdout
 });
 
+console.log("                      __________________");
+console.log("                      \\                 \\");
+console.log("                       \\                 \\");
+console.log("                        \\                 \\");
+console.log("                         \\                 \\");
+console.log("                          \\                 \\");
+console.log("                           \\                 \\");
+console.log("           /---------------//---------------------");
+console.log("          /---------------//                   / |");
+console.log("         /               //                   /__|");
+console.log("        /               //                   /   |");
+console.log("       /_______________//   o o             /    |");
+console.log("      /------------------------------------/     |");
+console.log("      |                                    |     |");
+console.log("      |                                    |     |");
+console.log("      |                                    |     |");
+console.log("      |------------------------------------|     |");
+
 readline.question('1-5: ', input => {
-
-  console.log("                      __________________");
-  console.log("                      \\                 \\");
-  console.log("                       \\                 \\");
-  console.log("                        \\                 \\");
-  console.log("                         \\                 \\");
-  console.log("                          \\                 \\");
-  console.log("                           \\                 \\");
-  console.log("           /---------------//---------------------");
-  console.log("          /---------------//                   / |");
-  console.log("         /               //                   /__|");
-  console.log("        /               //                   /   |");
-  console.log("       /_______________//   o o             /    |");
-  console.log("      /------------------------------------/     |");
-  console.log("      |                                    |     |");
-  console.log("      |                                    |     |");
-  console.log("      |                                    |     |");
-  console.log("      |------------------------------------|     |");
-
-
-
-
   console.log(`For you, recycle bin is ${input} out of 5!`);
   readline.close();
 });
